873915ab23d04a31f0d4092c64f318b8aa0d5223
Paulina Ng <qwnpng@gmail.com>
2016-12-05 05:45:04 +0000

    Added all chess pieces to board

--- chessboard.js
+++ chessboard.js
@@ -20,7 +20,23 @@ $('.square').on('click', function(evt) {
   });
 
 function setupPieces() {
-  $('#row-7>.square').html('<span class=\'chess-piece pawn\'>P</span>');
+  $('#row-7>.square').html('<span class=\'chess-piece pawn black\'>P</span>');
+  $('#row-2>.square').html('<span class=\'chess-piece pawn white\'>P</span>');
+
+  $('#row-8>.a, #row-8>.h').html('<span class=\'chess-piece rook black\'>R</span>');
+  $('#row-1>.a, #row-1>.h').html('<span class=\'chess-piece rook white\'>R</span>');
+
+  $('#row-8>.b, #row-8>.g').html('<span class=\'chess-piece knight black\'>N</span>');
+  $('#row-1>.b, #row-1>.g').html('<span class=\'chess-piece knight white\'>N</span>');
+
+  $('#row-8>.c, #row-8>.f').html('<span class=\'chess-piece bishop black\'>B</span>');
+  $('#row-1>.c, #row-1>.f').html('<span class=\'chess-piece bishop white\'>B</span>');
+
+  $('#row-8>.d').html('<span class=\'chess-piece queen black\'>Q</span>');
+  $('#row-1>.d').html('<span class=\'chess-piece queen white\'>Q</span>');
+
+  $('#row-8>.e').html('<span class=\'chess-piece king black\'>K</span>');
+  $('#row-1>.e').html('<span class=\'chess-piece king white\'>K</span>');
 }
 
 $('#new-game').on('click', setupPieces);
