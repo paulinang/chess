6085df942b2c1dfcee85f80f3ed662df389d9533
Paulina Ng <qwnpng@gmail.com>
2016-12-05 06:00:11 +0000

    Pieces are added directly to square divs instead of in a nested span, fixes problem where squares with pieces cannot be clicked

--- chessboard.js
+++ chessboard.js
@@ -20,23 +20,29 @@ $('.square').on('click', function(evt) {
   });
 
 function setupPieces() {
-  $('#row-7>.square').html('<span class=\'chess-piece pawn black\'>P</span>');
-  $('#row-2>.square').html('<span class=\'chess-piece pawn white\'>P</span>');
+  $('#row-7>.square').addClass('chess-piece pawn black');
+  $('#row-2>.square').addClass('chess-piece pawn white');
+  $('#row-7>.square, #row-2>.square').html('P');
 
-  $('#row-8>.a, #row-8>.h').html('<span class=\'chess-piece rook black\'>R</span>');
-  $('#row-1>.a, #row-1>.h').html('<span class=\'chess-piece rook white\'>R</span>');
+  $('#row-8>.a, #row-8>.h').addClass('chess-piece rook black');
+  $('#row-1>.a, #row-1>.h').addClass('chess-piece rook white');
+  $('#row-8>.a, #row-8>.h, #row-1>.a, #row-1>.h').html('R');
 
-  $('#row-8>.b, #row-8>.g').html('<span class=\'chess-piece knight black\'>N</span>');
-  $('#row-1>.b, #row-1>.g').html('<span class=\'chess-piece knight white\'>N</span>');
+  $('#row-8>.b, #row-8>.g').addClass('chess-piece knight black');
+  $('#row-1>.b, #row-1>.g').addClass('chess-piece knight white');
+  $('#row-8>.b, #row-8>.g, #row-1>.b, #row-1>.g').html('N');
 
-  $('#row-8>.c, #row-8>.f').html('<span class=\'chess-piece bishop black\'>B</span>');
-  $('#row-1>.c, #row-1>.f').html('<span class=\'chess-piece bishop white\'>B</span>');
+  $('#row-8>.c, #row-8>.f').addClass('chess-piece bishop black');
+  $('#row-1>.c, #row-1>.f').addClass('chess-piece bishop white');
+  $('#row-8>.c, #row-8>.f, #row-1>.c, #row-1>.f').html('B');
 
-  $('#row-8>.d').html('<span class=\'chess-piece queen black\'>Q</span>');
-  $('#row-1>.d').html('<span class=\'chess-piece queen white\'>Q</span>');
+  $('#row-8>.d').addClass('chess-piece queen black');
+  $('#row-1>.d').addClass('chess-piece queen white');
+  $('#row-8>.d, #row-1>.d').html('Q');
 
-  $('#row-8>.e').html('<span class=\'chess-piece king black\'>K</span>');
-  $('#row-1>.e').html('<span class=\'chess-piece king white\'>K</span>');
+  $('#row-8>.e').addClass('chess-piece king black');
+  $('#row-1>.e').addClass('chess-piece king white');
+  $('#row-8>.e, #row-1>.e').html('K');
 }
 
 $('#new-game').on('click', setupPieces);
